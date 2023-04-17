d9e8a8cfe5b6189986a4a22782bb98bed5271cdf
StevenTannerAllyis <100375006+StevenTannerAllyis@users.noreply.github.com>
2023-04-17 01:59:40 -0700

    Testing without file path or execution

--- KustoSpecialistLab6Downloads.ps1
+++ KustoSpecialistLab6Downloads.ps1
@@ -7,8 +7,9 @@
 
 #test
 $url = "https://www.microsoft.com/en-us/download/confirmation.aspx?id=58494&6B49FDFB-8E5B-4B07-BC31-15695C5A2143=1"
-$output = "C:\Users\student\Desktop\PBIDesktopSetup_x64.exe"
+#$output = "C:\Users\student\Desktop\PBIDesktopSetup_x64.exe"
+$output = "PBIDesktopSetup_x64.exe"
 
 Invoke-WebRequest -Uri $url -OutFile $output
-Start-Process -FilePath $output -ArgumentList "/quiet" -Wait
+#Start-Process -FilePath $output -ArgumentList "/quiet" -Wait
 #Remove-Item $output
