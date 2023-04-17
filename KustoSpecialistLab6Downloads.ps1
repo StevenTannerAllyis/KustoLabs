5bcec1849817327bdf34dd477d9383b5baede44b
StevenTannerAllyis <100375006+StevenTannerAllyis@users.noreply.github.com>
2023-04-17 01:44:23 -0700

    Testing a new way to download as exe

--- KustoSpecialistLab6Downloads.ps1
+++ KustoSpecialistLab6Downloads.ps1
@@ -1,13 +1,14 @@
-$url = "https://aka.ms/ke"
-$output = "downloadedAzureDataExplorer.msi"
+#$url = "https://aka.ms/ke"
+#$output = "downloadedAzureDataExplorer.msi"
 
-Invoke-WebRequest -Uri $url -OutFile $output
-Start-Process -FilePath $output -ArgumentList "/quiet" -Wait
-Remove-Item $output
+#Invoke-WebRequest -Uri $url -OutFile $output
+#Start-Process -FilePath $output -ArgumentList "/quiet" -Wait
+#Remove-Item $output
 
+#test
 $url = "https://www.microsoft.com/en-us/download/confirmation.aspx?id=58494&6B49FDFB-8E5B-4B07-BC31-15695C5A2143=1"
-$output = "PBIDesktopSetup_x64"
+$output = "C:\Users\student\Desktop\PBIDesktopSetup_x64.exe"
 
 Invoke-WebRequest -Uri $url -OutFile $output
 Start-Process -FilePath $output -ArgumentList "/quiet" -Wait
-Remove-Item $output
+#Remove-Item $output
