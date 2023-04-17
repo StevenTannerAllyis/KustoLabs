ae7db4a11befbc05fef6e885f3c6070d4361c8e5
StevenTannerAllyis <100375006+StevenTannerAllyis@users.noreply.github.com>
2023-04-17 00:50:53 -0700

    Changed output file name

--- KustoSpecialistLab6Downloads.ps1
+++ KustoSpecialistLab6Downloads.ps1
@@ -1,12 +1,12 @@
 $url = "https://aka.ms/ke"
-$output = "C:\AzureDataExplorer\downloadedAzureDataExplorer.msi"
+$output = "downloadedAzureDataExplorer.msi"
 
 Invoke-WebRequest -Uri $url -OutFile $output
 Start-Process -FilePath $output -ArgumentList "/quiet" -Wait
 Remove-Item $output
 
 $url = "https://www.microsoft.com/en-us/download/confirmation.aspx?id=58494&6B49FDFB-8E5B-4B07-BC31-15695C5A2143=1"
-$output = "C:\PowerBI\downloadedPowerBI.msi"
+$output = "PBIDesktopSetup_x64"
 
 Invoke-WebRequest -Uri $url -OutFile $output
 Start-Process -FilePath $output -ArgumentList "/quiet" -Wait
