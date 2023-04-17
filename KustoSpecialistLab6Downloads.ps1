0ab9304029817803be0dfeb56cb696427f81fcfc
StevenTannerAllyis <100375006+StevenTannerAllyis@users.noreply.github.com>
2023-04-17 03:35:31 -0700

    Downloaded file now gets moved to desktop

--- KustoSpecialistLab6Downloads.ps1
+++ KustoSpecialistLab6Downloads.ps1
@@ -1,18 +1,6 @@
-#$url = "https://aka.ms/ke"
-#$output = "downloadedAzureDataExplorer.msi"
-
-#Invoke-WebRequest -Uri $url -OutFile $output
-#Start-Process -FilePath $output -ArgumentList "/quiet" -Wait
-#Remove-Item $output
-
-#$url = "https://www.microsoft.com/en-us/download/confirmation.aspx?id=58494&6B49FDFB-8E5B-4B07-BC31-15695C5A2143=1"
-#$output = "PBIDesktopSetup_x64.exe"
-
 $url = "https://download.microsoft.com/download/8/8/0/880BCA75-79DD-466A-927D-1ABF1F5454B0/PBIDesktopSetup.exe"
 $output = "PBIDesktopSetup.exe"
 
 Invoke-WebRequest -Uri $url -OutFile $output
+Get-Job | Wait-Job
 Copy-Item "C:\Packages\Plugins\Microsoft.Compute.CustomScriptExtension\1.10.15\Downloads\0\PBIDesktopSetup.exe" -Destination "C:\Users\student\Desktop\PBIDesktopSetup.exe"
-
-#Start-Process -FilePath $output -ArgumentList "/quiet" -Wait
-#Remove-Item $output
